9998087a829317b79634bca785a5be973ea68095
Victor Tang <victortang1992@gmail.com>
2016-01-22 20:49:57 -0800

    min max algo with reduce

--- array_getMax.js
+++ array_getMax.js
@@ -8,3 +8,20 @@ function getMax(arr){
 }
 
 getMax([1,500,600,1]);
+
+
+//returning an object with reduce
+
+getMinMax([1,500,600,1]);
+
+function getMinMax(arr){
+    return arr.reduce(function(obj,next){
+        if(obj.min>next){
+            obj.min=next;
+        }
+        if(obj.max<next){
+            obj.max=next;
+        }
+        return obj;
+    },{min:Infinity,max:-Infinity});
+}
