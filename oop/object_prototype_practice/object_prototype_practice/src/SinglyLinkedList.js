0bc7a1e666b12d0f41434c68c54e50df8028d15e
Victor Tang <victortang1992@gmail.com>
2016-05-16 21:44:10 -0700

    singly linked list tests

--- oop/object_prototype_practice/object_prototype_practice/src/SinglyLinkedList.js
+++ oop/object_prototype_practice/object_prototype_practice/src/SinglyLinkedList.js
@@ -14,30 +14,59 @@ SinglyLinkedList.prototype.add = function(name,value){
         //create a new item object, place data in
         var node = this.content[name] = new LinkedListNode(value);
 
-        //start list
-        if (this._length === 0) {
-            this._tail = node[name];
+        //attach at the end
+        if (this._tail) {
+            this._tail.next = node;
+            this._tail = node;
         }
 		else {
-            //attach at the end
-            this._tail.next = node[name];
-            this._tail = node[name];
+			//start list
+            this._tail = node;
         }
 
 };
 
 SinglyLinkedList.prototype.remove = function(name){
 
-    var nextNode = this.content.name.next;
+	var prevNode = this.content.findPrev(function(node){
+		return node.next = this.content[name];
+	});
+    var nextNode = this.content[name].next;
 
-	if(nextNode){
-		//target, next
-		this.content.name.value = nextNode.value;
-		this.content.name.next = nextNode.next;
-	}
+    if (prevNode) {
+		if(nextNode){
+			//prev, target, next
+			prevNode.next = nextNode;
+			nextNode.prev = prevNode;
+		}
+		else{
+			//prev, target
+			prevNode.next = null;
+			this._tail = prevNode;
+		}
+    }
 	else{
-		//target
-		this._tail = null;
+		if (nextNode) {
+			//target, next
+			nextNode.prev = null;
+		}
+		else{
+			//target
+			this._tail = null;
+		}
 	}
-	delete this.content.name;
+	delete this.content[name];
 };
+
+SinglyLinkedList.prototype.get = function(name){
+
+	return this.content[name];
+};
+
+Object.prototype.findPrev = function( test ) {
+    for( var prop in this ) {
+		if( test ){
+			return this[prop];
+		}
+    }
+}
--- oop/object_prototype_practice/object_prototype_practice/test/SinglyLinkedList.js
+++ oop/object_prototype_practice/object_prototype_practice/test/SinglyLinkedList.js
@@ -1,21 +1,89 @@
 describe('Singly Linked List', function() {
 
-  verifyClass(SinglyLinkedList).followsPattern('pseudoclassical', {}, false);
+	verifyClass(SinglyLinkedList).followsPattern('pseudoclassical', {}, false);
 
-  beforeEach(function() {
-    list = new SinglyLinkedList();
-  });
+	beforeEach(function() {
+	list = new SinglyLinkedList();
+	});
 
-  it('should be empty from the start', function() {
-    expect(list.content).to.deep.equal({});
-  });
+	it('should be empty from the start', function() {
+	expect(list.content).to.deep.equal({});
+	});
 
-  it('should definitely have no head', function() {
-    expect(list._head).to.equal(undefined);
-  });
+	it('should definitely have no head', function() {
+	expect(list._head).to.equal(undefined);
+	});
 
-  it('should have no tail', function() {
-    expect(list._tail).to.equal(null);
-  });
+	it('should have no tail', function() {
+	expect(list._tail).to.equal(null);
+	});
 
+	describe('Adding and removing', function() {
+
+		beforeEach(function() {
+			list.add('a',"this is value for 'a'");
+		});
+
+		it('you can add a value and access it later', function() {
+			expect(list.get('a').value).to.equal("this is value for 'a'");
+		});
+
+		it('you can add a value and remove it', function() {
+			list.remove('a');
+			expect(list.get('a')).to.equal(undefined);
+		});
+	});
+
+	describe('Adding and removing ({a,b})', function() {
+
+		beforeEach(function() {
+			list.add('a',"this is value for 'a'");
+			list.add('b',"this is value for 'b'");
+		});
+
+		it('b is tail', function() {
+			expect(list._tail.value).to.equal("this is value for 'b'");
+		});
+		
+		it('you can get value for b', function() {
+			expect(list.get('b').value).to.equal("this is value for 'b'");
+		});
+
+		it('you can get value for b after removing a, and b would still be tail', function() {
+			list.remove('a');
+			expect(list.get('a')).to.equal(undefined);
+			expect(list.get('b').value).to.equal("this is value for 'b'");
+			expect(list._tail.value).to.equal("this is value for 'b'");
+		});
+
+		it('you can remove b and make a the tail without a next node', function() {
+			list.remove('b');
+			expect(list.get('b')).to.equal(undefined);
+			expect(list.get('a').next).to.equal(null);
+			expect(list._tail.value).to.equal("this is value for 'a'");
+		});
+	});
+
+	describe('Adding and removing ({a,b})', function() {
+
+		beforeEach(function() {
+			list.add('a',"this is value for 'a'");
+			list.add('b',"this is value for 'b'");
+		});
+
+		it('b is tail', function() {
+			expect(list._tail.value).to.equal("this is value for 'b'");
+		});
+		
+		it('you can get value for b', function() {
+			expect(list.get('b').value).to.equal("this is value for 'b'");
+		});
+
+		it('you can get value for b after removing a, and b would still be tail', function() {
+			list.remove('a');
+			expect(list.get('a')).to.equal(undefined);
+			expect(list.get('b').value).to.equal("this is value for 'b'");
+			expect(list._tail.value).to.equal("this is value for 'b'");
+		});
+	});
 });
