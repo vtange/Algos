5793184c654fe76e30e1f34b9cfe838477df1de4
Victor Tang <victortang1992@gmail.com>
2016-05-26 00:54:36 -0700

    depth searcher

--- oop/object_prototype_practice/object_prototype_practice/src/BinHeap.js
+++ oop/object_prototype_practice/object_prototype_practice/src/BinHeap.js
@@ -1,5 +1,4 @@
 function TreeNode(value){
-	this.parent = null;
 	this.value = value;
 	this.left = null;
 	this.right = null;
@@ -12,6 +11,10 @@ function BinaryHeap(value){
 	}
 	this.root = this.content[0] || null;
 }
+
+/*-----*/
+/* Breadth First Search */
+/*-----*/
 
 BinaryHeap.prototype.add = function(value){
 	var tree = this.content;
@@ -32,12 +35,13 @@ BinaryHeap.prototype.add = function(value){
 			}
 			else{
 				if(tree[currBatch[i]].value > hand){
+					//swap current value of tree with one on hand
 					tree[currBatch[i]].value = [hand, hand = tree[currBatch[i]].value][0];
 				}
 			}
 			//prepare next batch
-			nextBatch.push((2*currBatch[i])+1)
-			nextBatch.push((2*currBatch[i])+2)
+			nextBatch.push((2*currBatch[i])+1);
+			nextBatch.push((2*currBatch[i])+2);
 		};
 		currBatch = nextBatch;
 	}
@@ -69,4 +73,21 @@ BinaryHeap.prototype.get = function(value){
 		};
 		currBatch = nextBatch;
 	}	
+}
+
+/*-----*/
+/* Depth First Search */
+/*-----*/
+
+function traverse(node,method);
+	var next = [];
+	
+;
+
+function processNode(node,[]){
+	[].push(node.value)
+	if(node.left)
+		next.push(node.left);
+	if(node.right)
+		next.push(node.right);
 }
