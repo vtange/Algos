f53c253ea5c4202bfa0712c0f578016a92bd005a
Victor Tang <victortang1992@gmail.com>
2016-08-10 23:12:05 -0700

    add backend deploy notes

---
+++ backend/deploy.js
@@ -0,0 +1,41 @@
+/*--------------*/
+/*  HOW TO HEROKU  */
+/*--------------*/
+/*
+
+heroku create <name>
+	create a new app
+
+heroku open
+	open app's URL in browser
+
+
+ex. heroku apps:rename <new name>
+heroku apps:______________
+	:rename <new name>
+	:destroy --app
+	
+	
+heroku logs
+	show logs for debug
+
+
+/*--------------*/
+/*  HOW TO AWS (EB)  */
+/*--------------*/
+/*
+ 
+eb list
+ 	list eb environments related to project
+
+eb init
+ 	initiate eb project setting (similar to npm)
+
+eb create
+	create a new environment for project
+
+eb deploy
+	
+eb logs
+ 
+*/
